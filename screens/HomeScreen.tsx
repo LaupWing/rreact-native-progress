5c0875faf2f81fc7a7e10c120110bdee985d373b
LaupWing <loc_strict@hotmail.com>
2023-02-21 11:41:02 +0100

    aai

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,23 @@
 import { View, Text } from "react-native"
 import { Ionicons } from "@expo/vector-icons"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
 
 const HomeScreen = () => {
+   const insets = useSafeAreaInsets()
    return (
-      <View className="flex-1">
-         <View className="px-4 flex flex-row items-center border-b border-gray-300">
-            <Ionicons name="menu" size={28} color="black" />
-            <Text className="text-3xl">50%</Text>
-            <View className="border-purple-500 border-2 flex-1 rounded-full p-0.5 ml-2">
-               <View className="w-[48%] h-2 bg-purple-500 rounded-full"></View>
+      <View 
+         className="flex-1"
+         style={{
+            paddingTop: insets.top
+         }}
+      >
+         <View className="flex-1">
+            <View className="px-4 py-3 flex flex-row items-center border-b border-gray-300">
+               <Ionicons name="menu" size={28} color="black" />
+               <Text className="text-3xl">50%</Text>
+               <View className="border-purple-500 border-2 flex-1 rounded-full p-0.5 ml-2">
+                  <View className="w-[48%] h-2 bg-purple-500 rounded-full"></View>
+               </View>
             </View>
          </View>
       </View>
