88d9ce1b60fa1299bd4b9692c4be9e31a10aa91b
LaupWing <loc_strict@hotmail.com>
2023-02-21 11:27:48 +0100

    RootStackParamsList

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
-import { StatusBar } from "expo-status-bar"
-import { Text, View } from "react-native"
-import { Ionicons } from "@expo/vector-icons"
 import { NavigationContainer } from "@react-navigation/native"
 import { SafeAreaProvider } from "react-native-safe-area-context"
+
+export type RootStackParamsList = {
+   Home: undefined
+   Login: undefined
+}
 
 export default function App() {
    return (
       <NavigationContainer>
          <SafeAreaProvider className="bg-white flex-1">
-            <View className="py-10 px-4 flex flex-row items-center border-b border-gray-300">
-               <Ionicons name="menu" size={28} color="black" />
-               <Text className="text-3xl">50%</Text>
-               <View className="border-purple-500 border-2 flex-1 rounded-full p-0.5 ml-2">
-                  <View className="w-[48%] h-2 bg-purple-500 rounded-full"></View>
-               </View>
-            </View>
-            <StatusBar style="auto" />
+            
          </SafeAreaProvider>
       </NavigationContainer>
    )
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,16 @@
 import { View, Text } from "react-native"
+import { Ionicons } from "@expo/vector-icons"
 
 const HomeScreen = () => {
    return (
       <View>
-         <Text>HomeScreen</Text>
+         <View className="py-10 px-4 flex flex-row items-center border-b border-gray-300">
+            <Ionicons name="menu" size={28} color="black" />
+            <Text className="text-3xl">50%</Text>
+            <View className="border-purple-500 border-2 flex-1 rounded-full p-0.5 ml-2">
+               <View className="w-[48%] h-2 bg-purple-500 rounded-full"></View>
+            </View>
+         </View>
       </View>
    )
 }
