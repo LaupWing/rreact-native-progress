f67c0ed8d293976b530efe8bfb315dc9314160f5
LaupWing <loc_strict@hotmail.com>
2023-02-21 11:09:05 +0100

    update

--- App.tsx
+++ App.tsx
@@ -4,9 +4,11 @@ import { SafeAreaView, Text, View } from "react-native"
 export default function App() {
    return (
       <SafeAreaView className="bg-white flex-1">
-         <View className="py-10 px-4 flex flex-row">
+         <View className="py-10 px-4 flex flex-row items-center">
             <Text className="text-3xl">50%</Text>
-            <View className=""></View>
+            <View className="border-purple-500 border-2 flex-1 rounded-full p-0.5 ml-2">
+               <View className="w-[48%] h-2 bg-purple-500 rounded-full"></View>
+            </View>
          </View>
          <StatusBar style="auto" />
       </SafeAreaView>
