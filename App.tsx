b412db0a539e32950ecdbfbfe8ec376edafc2c1d
LaupWing <loc_strict@hotmail.com>
2023-02-21 11:19:26 +0100

    udpate

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
 import { StatusBar } from "expo-status-bar"
 import { SafeAreaView, Text, View } from "react-native"
 import { Ionicons } from "@expo/vector-icons"
+import { NavigationContainer } from "@react-navigation/native"
+import { SafeAreaProvider } from "react-native-safe-area-context"
 
 export default function App() {
    return (
-      <SafeAreaView className="bg-white flex-1">
-         <View className="py-10 px-4 flex flex-row items-center border-b border-gray-300">
-            <Ionicons name="menu" size={28} color="black" />
-            <Text className="text-3xl">50%</Text>
-            <View className="border-purple-500 border-2 flex-1 rounded-full p-0.5 ml-2">
-               <View className="w-[48%] h-2 bg-purple-500 rounded-full"></View>
+      <NavigationContainer>
+         <SafeAreaProvider className="bg-white flex-1">
+            <View className="py-10 px-4 flex flex-row items-center border-b border-gray-300">
+               <Ionicons name="menu" size={28} color="black" />
+               <Text className="text-3xl">50%</Text>
+               <View className="border-purple-500 border-2 flex-1 rounded-full p-0.5 ml-2">
+                  <View className="w-[48%] h-2 bg-purple-500 rounded-full"></View>
+               </View>
             </View>
-         </View>
-         <StatusBar style="auto" />
-      </SafeAreaView>
+            <StatusBar style="auto" />
+         </SafeAreaProvider>
+      </NavigationContainer>
    )
 }
