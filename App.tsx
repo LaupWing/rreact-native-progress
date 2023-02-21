0b0717d79e8bf5f7c7ce5241966ce9162dc8c3c0
LaupWing <loc_strict@hotmail.com>
2023-02-21 11:14:11 +0100

    udpate

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar"
 import { SafeAreaView, Text, View } from "react-native"
+import { Ionicons } from "@expo/vector-icons"
 
 export default function App() {
    return (
       <SafeAreaView className="bg-white flex-1">
-         <View className="py-10 px-4 flex flex-row items-center">
+         <View className="py-10 px-4 flex flex-row items-center border-b border-gray-300">
+            <Ionicons name="menu" size={28} color="black" />
             <Text className="text-3xl">50%</Text>
             <View className="border-purple-500 border-2 flex-1 rounded-full p-0.5 ml-2">
                <View className="w-[48%] h-2 bg-purple-500 rounded-full"></View>
