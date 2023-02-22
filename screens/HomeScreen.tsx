85478f3197bd8a2963cc43ee42b13544ea17307d
LaupWing <loc_strict@hotmail.com>
2023-02-22 09:42:41 +0100

    some spacing at the top

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -13,8 +13,8 @@ const HomeScreen = () => {
       >
          <View className="flex-1">
             <View className="px-4 py-3 flex flex-row items-center border-b border-gray-300">
-               <Ionicons name="menu" size={28} color="black" />
-               <Text className="text-3xl">50%</Text>
+               <Ionicons name="menu" size={30} color="black" />
+               <Text className="text-2xl mx-2">50%</Text>
                <View className="border-purple-500 border-2 flex-1 rounded-full p-0.5 ml-2">
                   <View className="w-[48%] h-2 bg-purple-500 rounded-full"></View>
                </View>
