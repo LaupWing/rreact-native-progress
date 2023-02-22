95f652313f023d533cb5df85d384bef8269b144d
LaupWing <loc_strict@hotmail.com>
2023-02-22 09:46:50 +0100

    udpate

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,11 @@
-import { View, Text } from "react-native"
+import { View, Text, FlatList } from "react-native"
 import { Ionicons } from "@expo/vector-icons"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
+import {tasks} from "../data"
 
 const HomeScreen = () => {
    const insets = useSafeAreaInsets()
+   
    return (
       <View 
          className="flex-1"
@@ -18,6 +20,18 @@ const HomeScreen = () => {
                <View className="border-purple-500 border-2 flex-1 rounded-full p-0.5 ml-2">
                   <View className="w-[48%] h-2 bg-purple-500 rounded-full"></View>
                </View>
+            </View>
+            <View>
+               <FlatList
+                  data={tasks}
+                  renderItem={({item}) => {
+                     return (
+                        <View className="px-3 py-2 border-b border-gray-300">
+                           <Text className="text-lg">{item.text}</Text>
+                        </View>
+                     )
+                  }}
+               />
             </View>
          </View>
       </View>
