136746abbc3f6d972c045ed67f3a8c63d1b4dbe7
LaupWing <loc_strict@hotmail.com>
2023-02-24 09:49:21 +0100

    update

---
+++ screens/MonthScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+const MonthScreen = () => {
+   return (
+      <View>
+         <Text>MonthScreen</Text>
+      </View>
+   )
+}
+export default MonthScreen
---
+++ screens/WeeksScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+const WeeksScreen = () => {
+   return (
+      <View>
+         <Text>WeeksScreen</Text>
+      </View>
+   )
+}
+export default WeeksScreen
