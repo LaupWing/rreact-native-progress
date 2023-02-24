2fe212a5feb7072f6f1bf6f09990b04dff3238f8
LaupWing <loc_strict@hotmail.com>
2023-02-24 10:01:04 +0100

    udpate

--- App.tsx
+++ App.tsx
@@ -1,30 +1,33 @@
 import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { SafeAreaProvider } from "react-native-safe-area-context"
 import HomeScreen from "./screens/HomeScreen"
+import "react-native-gesture-handler"
+import { createDrawerNavigator } from "@react-navigation/drawer"
+import MonthScreen from "./screens/MonthScreen"
+import WeeksScreen from "./screens/WeeksScreen"
 
 export type RootStackParamsList = {
    Home: undefined
-   Login: undefined
+   Month: undefined
+   Week: undefined
 }
 
-const Stack = createNativeStackNavigator<RootStackParamsList>()
+const Drawer = createDrawerNavigator<RootStackParamsList>()
 
 export default function App() {
    return (
       <SafeAreaProvider>
          <NavigationContainer>
-            <Stack.Navigator
+            <Drawer.Navigator
                screenOptions={{
-                  headerShown: false
+                  headerShown: false,
                }}
                initialRouteName="Home"
             >
-               <Stack.Screen
-                  name="Home"
-                  component={HomeScreen}
-               />
-            </Stack.Navigator>
+               <Drawer.Screen name="Home" component={HomeScreen} />
+               <Drawer.Screen name="Month" component={MonthScreen} />
+               <Drawer.Screen name="Week" component={WeeksScreen} />
+            </Drawer.Navigator>
          </NavigationContainer>
       </SafeAreaProvider>
    )
