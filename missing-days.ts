2a3721e09ea083fd71a94268c7c53f5900f8244a
LaupWing <loc_strict@hotmail.com>
2023-03-06 10:00:09 +0100

    can get max now

--- missing-days.ts
+++ missing-days.ts
@@ -1,5 +1,7 @@
 import {tasks} from "./data"
 import moment from "moment"
 
-console.log(Object.keys(tasks[0].dates).map(date => moment(date)))
-// console.log(moment.max(Object.keys(tasks[0].dates).map(date => moment(date))))
+
+const formattedDates = Object.keys(tasks[0].dates).map(date => moment(date, "DD-MM-YYYY"))
+
+console.log(moment.max(formattedDates))
