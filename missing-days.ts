cd06358f95fec3e6a3fca3950fe4d199c6dfb1db
LaupWing <loc_strict@hotmail.com>
2023-03-06 10:23:04 +0100

    got this

--- missing-days.ts
+++ missing-days.ts
@@ -1,15 +1,20 @@
 import {tasks} from "./data"
 import Moment from "moment"
 import { extendMoment } from "moment-range"
-import diff from "fast-array-diff"
 
 // @ts-ignore
 const moment = extendMoment(Moment)
-
-const formattedDates = Object.keys(tasks[0].dates).map(date => moment(date, "DD-MM-YYYY"))
+const formatStyle = "DD-MM-YYYY"
+const formattedDates = Object.keys(tasks[0].dates).map(date => moment(date, formatStyle))
 
 const maxium_date = moment.max(formattedDates)
 const begin_date = moment.min(formattedDates)
 const range = moment.range(begin_date, maxium_date)
 
-console.log(diff.diff(Array.from(range.by("days")), formattedDates))
+const all_available = Array.from(range.by("days")).every((date) => {
+   return formattedDates.find(x => moment(x).format(formatStyle) === moment(date).format(formatStyle))
+})
+
+const range2 = moment.range(maxium_date, moment())
+console.log(Array.from(range2.by("days")).map(x => moment(x).format(formatStyle)))
+// console.log(diff.diff(, formattedDates))
