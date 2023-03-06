ed31946863f4a9d7e18e498aca3d16744abd75eb
LaupWing <loc_strict@hotmail.com>
2023-03-06 10:31:19 +0100

    update

--- missing-days.ts
+++ missing-days.ts
@@ -7,9 +7,9 @@ const moment = extendMoment(Moment)
 const formatStyle = "DD-MM-YYYY"
 const formattedDates = Object.keys(tasks[0].dates).map(date => moment(date, formatStyle))
 
-const maxium_date = moment.max(formattedDates)
+const latest_date = moment.max(formattedDates)
 const begin_date = moment.min(formattedDates)
-const range = moment.range(begin_date, maxium_date)
+const range = moment.range(begin_date, latest_date)
 
 const all_available = Array.from(range.by("days")).every((date) => {
    return formattedDates.find(x => moment(x).format(formatStyle) === moment(date).format(formatStyle))
@@ -22,6 +22,7 @@ if(!all_available){
    console.log(not_available)
 }
 
-const range2 = moment.range(maxium_date, moment())
-console.log(Array.from(range2.by("days")).map(x => moment(x).format(formatStyle)))
+const range2 = moment.range(latest_date, moment())
+const from_max_to_now =  Array.from(range2.by("days")).map(x => moment(x).format(formatStyle))
+
 // console.log(diff.diff(, formattedDates))
