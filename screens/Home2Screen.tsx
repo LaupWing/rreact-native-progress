dabc3e71403c949994fba04f1e06c3d982c78686
LaupWing <loc_strict@hotmail.com>
2023-03-07 09:57:55 +0100

    updater

---
+++ screens/Home2Screen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+const Home2Screen = () => {
+   return (
+      <View>
+         <Text>Home2Screen</Text>
+      </View>
+   )
+}
+export default Home2Screen
