64f15ef57cb6efa04f0334a147187afbb0605f49
LaupWing <loc_strict@hotmail.com>
2023-03-07 10:16:39 +0100

    Login page

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -11,7 +11,7 @@ export type RootStackParamsList = {
 
 const Drawer = createDrawerNavigator<RootStackParamsList>()
 
-const Home2Screen = () => {
+const HomeScreen = () => {
    return (
       <Drawer.Navigator
          screenOptions={{
@@ -26,4 +26,4 @@ const Home2Screen = () => {
       </Drawer.Navigator>
    )
 }
-export default Home2Screen
+export default HomeScreen
