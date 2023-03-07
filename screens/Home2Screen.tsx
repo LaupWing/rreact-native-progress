d08598f8badb37e72bdccb88d775e516b80ceeda
LaupWing <loc_strict@hotmail.com>
2023-03-07 10:02:03 +0100

    HOme screen

--- screens/Home2Screen.tsx
+++ screens/Home2Screen.tsx
@@ -1,9 +1,35 @@
-import { View, Text } from "react-native"
+import { createDrawerNavigator } from "@react-navigation/drawer"
+import { NavigationContainer } from "@react-navigation/native"
+import { SafeAreaProvider } from "react-native-safe-area-context"
+import HomeScreen from "./HomeScreen"
+import MonthScreen from "./MonthScreen"
+import WeeksScreen from "./WeeksScreen"
+
+export type RootStackParamsList = {
+   Home: undefined
+   Month: undefined
+   Week: undefined
+}
+
+const Drawer = createDrawerNavigator<RootStackParamsList>()
+
 const Home2Screen = () => {
    return (
-      <View>
-         <Text>Home2Screen</Text>
-      </View>
+      <SafeAreaProvider>
+         <NavigationContainer>
+            <Drawer.Navigator
+               screenOptions={{
+                  drawerType: "front",
+                  headerShown: false,
+                  drawerActiveTintColor: "#A855F7"
+               }}
+            >
+               <Drawer.Screen name="Home" component={HomeScreen} />
+               <Drawer.Screen name="Month" component={MonthScreen} />
+               <Drawer.Screen name="Week" component={WeeksScreen} />
+            </Drawer.Navigator>
+         </NavigationContainer>
+      </SafeAreaProvider>
    )
 }
 export default Home2Screen
