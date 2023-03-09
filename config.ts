d34bc58aa8d248977fa2ada9fdc091013596ed75
LaupWing <loc_strict@hotmail.com>
2023-03-09 14:19:38 +0100

    got the weeks

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export default {
    test_account: "",
-   date_formt: "DD-MM-YYYY"
+   date_format: "DD-MM-YYYY"
 }
--- screens/WeeksScreen.tsx
+++ screens/WeeksScreen.tsx
@@ -1,9 +1,33 @@
-import { View, Text } from "react-native"
+import type {Moment} from "moment"
+import moment from "moment"
+import { View, Text} from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+import HomeHeader from "../components/HomeHeader"
+import config from "../config"
+import {tasks} from "../data"
+
 const WeeksScreen = () => {
+   const weeks = Object.keys(tasks[0].dates)
+      .map(date => moment(date, config.date_format))
+      .sort((left:Moment, right:Moment) => {
+         return moment.utc(left).diff(moment.utc(right))
+      })
+      .map(d => d.isoWeek()) 
+      .filter((value, index, array)=> {
+         return array.indexOf(value) === index;
+       })
+   console.log(weeks)
    return (
-      <View className="flex-1">
-         <Text>WeeksScreen</Text>
-      </View>
+      <SafeAreaView className="flex-1 bg-white">
+         <View className="flex-1">
+            <HomeHeader>
+               <Text className="text-2xl mx-2">50%</Text>
+               <View className="border-purple-500 border-2 flex-1 rounded-full p-0.5 ml-2">
+                  <View className="w-[48%] h-2 bg-purple-500 rounded-full"></View>
+               </View>
+            </HomeHeader>
+         </View>
+      </SafeAreaView>
    )
 }
 export default WeeksScreen
