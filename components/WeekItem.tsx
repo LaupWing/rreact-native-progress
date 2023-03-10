14305819957818f6ef7193031f32708036830dd4
LaupWing <loc_strict@hotmail.com>
2023-03-10 14:06:42 +0100

    update

--- components/WeekItem.tsx
+++ components/WeekItem.tsx
@@ -29,22 +29,24 @@ const WeekItem:FC<WeekItemProps> = ({
             finished:task.dates[dateKey]
          }
       })
-
+   const progress = Math.round((_tasks.filter(x=>x.finished).length / _tasks.length) * 100)
    return (
-      <View className="px-3 py-2 border-b border-gray-300 w-full">
-         <View className="flex-row flex-1 justify-between">
-            <View className="flex flex-row space-x-2">
-               <Text className="text-lg">{year}</Text>
-               <Text className="text-lg">{weekNumber}</Text>
+      <>
+         {progress ? (<View className="px-3 py-2 border-b border-gray-300 w-full">
+            <View className="flex-row flex-1 justify-between">
+               <View className="flex flex-row space-x-2">
+                  <Text className="text-lg">{year}</Text>
+                  <Text className="text-lg">{weekNumber}</Text>
+               </View>
+               <Feather name="chevron-down" size={24} color="black" />
             </View>
-            <Feather name="chevron-down" size={24} color="black" />
-         </View>
-         <View>
-            {_tasks.map(x => (
-               <Text className={x.finished ? "text-green-500" : "text-red-400"}>{x.text}</Text>
-            ))}
-         </View>
-      </View>
+            {/* <View>
+               {_tasks.map(x => (
+                  <Text className={x.finished ? "text-green-500" : "text-red-400"}>{x.text}</Text>
+               ))}
+            </View> */}
+         </View>) : null}
+      </>
    )
 }
 export default WeekItem
